4d3f09b8eba8605632daeae8b54d40884a6f7c67
DRyhlov <dryhlov@neoflex.ru>
2020-07-30 13:26:49 +0200

    Реализация кросс-платформенности

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,6 +12,11 @@ import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
+    private static final String
+    PLATFORM_IOS="ios",
+    PLATFORM_ANDROID="android"
+            ;
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
@@ -19,18 +25,9 @@ public class CoreTestCase extends TestCase {
 
         super.setUp();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Users\\vsary\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
-        capabilities.setCapability("orientation","PORTRAIT");
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
+        AppiumDriver driver = this.getDriverByPlatformEnv();
+        this.rotateScreenPortrait();
 
     }
 
@@ -54,5 +51,58 @@ public class CoreTestCase extends TestCase {
     protected void backgroundApp(Duration seconds) {
 
         driver.runAppInBackground(seconds);
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "8.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "C:\\Users\\vsary\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+            capabilities.setCapability("orientation", "PORTRAIT");
+
+        } else if (platform.equals(PLATFORM_IOS)) {
+
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone 8");
+            capabilities.setCapability("platformVersion", "13.6");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("app", "/Users/karma/Documents/GitHub/JavaAppiumAutomation/apks/Wikipedia.app");
+            capabilities.setCapability("orientation", "PORTRAIT");
+        } else {
+            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        }
+
+        return capabilities;
+    }
+
+    private AppiumDriver getDriverByPlatformEnv() throws Exception {
+
+        String platform = System.getenv("PLATFORM");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        }
+        else if (platform.equals(PLATFORM_IOS)) {
+            driver = new IOSDriver(new URL(AppiumURL),capabilities);
+        }
+        else {
+            throw new Exception("Cannot get run driver from env variable. Driver value " + driver);
+        }
+
+        return driver;
     }
 }

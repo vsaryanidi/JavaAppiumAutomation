d5ad37c956a019f188ac21915ae34ea2caa47e1e
DRyhlov <dryhlov@neoflex.ru>
2020-07-30 12:02:53 +0200

    Доп.правки локаторов

--- src/lib/ui/WelcomePageObject.java
+++ src/lib/ui/WelcomePageObject.java
@@ -7,12 +7,12 @@ public class WelcomePageObject extends MainPageObject {
 
     private static final String
 
-    STEP_LEARN_MORE_LINK="//XCUIElementTypeStaticText[@name=\"Learn more about Wikipedia\"]",
+    STEP_LEARN_MORE_LINK="//XCUIElementTypeStaticText[@name='Learn more about Wikipedia']",
     STEP_NEW_WAYS_TO_EXPLORE_TEXT="New ways to explore",
-    STEP_ADD_OR_PREFERRED_LANG_LINK="//XCUIElementTypeStaticText[@name=\"Add or edit preferred languages\"]",
-    STEP_LEARN_MORE_ABOUT_DATA_COLLECTED="//XCUIElementTypeStaticText[@name=\"Learn more about data collected\"]",
-    NEXT_LINK="//XCUIElementTypeStaticText[@name=\"Next\"]",
-    GET_STARTED_BUTTON="//XCUIElementTypeStaticText[@name=\"Get started\"]"
+    STEP_ADD_OR_PREFERRED_LANG_LINK="//XCUIElementTypeStaticText[@name='Add or edit preferred languages']",
+    STEP_LEARN_MORE_ABOUT_DATA_COLLECTED="//XCUIElementTypeStaticText[@name='Learn more about data collected']",
+    NEXT_LINK="//XCUIElementTypeButton[@name='Next']",
+    GET_STARTED_BUTTON="//XCUIElementTypeStaticText[@name='Get started']"
     ;
 
     public WelcomePageObject(AppiumDriver driver) {
@@ -28,17 +28,13 @@ public class WelcomePageObject extends MainPageObject {
                 "Cannot find 'Learn more about Wikipedia' link",
                 10);
 
-        System.out.println(STEP_LEARN_MORE_LINK);
 
     }
 
 
     public void clickNextButton() {
 
-        int size = driver.findElements(By.xpath(NEXT_LINK)).size();
-        System.out.println(size);
-
-        this.waitForElementAndClick(
+      this.waitForElementAndClick(
                 By.xpath(NEXT_LINK),
                 "Cannot find and click button 'Next'",
                 10);
